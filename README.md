1a52bef2cbdd18329718089676f339f9e41ed114
叫我小朋友 <alexchan1st@163.com>
2022-06-29 16:31:17 +0800

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# Rebr (rebr)
+
+A electron-based web-browser which is able to record the associated HTTP requests according to the configuration.
+
+## Install the dependencies
+```bash
+yarn
+# or
+npm install
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+npm run dev
+```
+
+
+### Build the app for production
+```bash
+npm run build
+```
+
+### Customize the configuration
+See [Configuring quasar.config.js](https://v2.quasar.dev/quasar-cli-vite/quasar-config-js).
---
+++ src-electron/msg-center.js
@@ -0,0 +1,43 @@
+import { app } from 'electron'
+import * as log from "./lib/log";
+import * as utils from "./lib/utils";
+// const fs = require("fs");
+// const path = require("path");
+const moment = require("moment");
+let send2win = utils.send2win;
+
+/**
+ *  ipc 进程间消息中转中心
+ */
+async function bindMsgCenter(ipcMain, windows) {
+    let mainWindow = windows.mainWindow;
+
+    ipcMain.on("toMain", async (event, args) => {
+        let source = args["source"];
+        if (mainWindow === null) {
+            // app.quit();
+            return;
+        }
+        switch (source) {
+            case "main":
+                send2win(mainWindow, { msg: "mainReceive" });
+                let type = "", key = "", cache = "";
+                log.operation.info("main", args);
+                switch (args["msg"]) {
+                    case "getAssetsPath":
+                        send2win(mainWindow, {
+                            msg: "assetsPaths", data: {
+                                logSrc: utils.getLogPath(),
+                                dataSrc: utils.getDataSavePath()
+                            }
+                        });
+                        break;
+                }
+                break;
+        }
+    });
+}
+
+export {
+    bindMsgCenter
+}
